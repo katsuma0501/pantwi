049aa4d64e9d2b4018815377f994b62b28471fa3
小林克麻 <k-kobayashi@moonfactory.co.jp>
2017-12-15 19:12:26 +0900

    twitter読み込み

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <link rel="apple-touch-icon-precomposed apple-touch-icon" href="" />
 <link rel="shortcut icon" href="" />
 <link rel="home" href="" title="" />
-<title>パンツイ</title>
+<title>パンとツイート</title>
 
 <!--[if lt IE 10]>
 <meta http-equiv="refresh" content="0; URL=oldie.html">
@@ -33,7 +33,15 @@ var setLatLon = function(lat, lon){
 		data: {
 			lat: lat,
 			lon: lon,
-			lists: []
+			lists: [],
+			word: ""
+		}
+	});
+	var twi_lists = new Vue({
+		el: '#twi_lists',
+		data: {
+			twi_word: [],
+			twi_list: []
 		}
 	});
 	var app = new Vue({
@@ -47,7 +55,46 @@ var setLatLon = function(lat, lon){
 			.then(function (response) {
 				console.log(response);				
 				lists.lists = response["data"]["rest"];
+
+				var rest = response["data"]["rest"];
+
 				lists.$forceUpdate();
+
+				for( var r  in rest ){
+					// twi_lists.twi_word.push(rest[r]['name']);
+					axios.get("./twi.php?q=" + rest[r]['name'],{'responseType':'json'})
+						.then(function (response) {
+							console.log(response);	
+							for( var d  in response["data"] ){
+								twi_lists.twi_list.push({
+									text:response["data"][d]["text"],
+									user_name:response["data"][d]["user"]["name"]
+								})
+							}
+							// twi_lists.twi_list = response["data"];
+						})
+						.catch(function (error) {
+							console.log(error);
+						});
+				}
+
+				twi_lists.$forceUpdate();
+
+				// var twi_search_word = twi_lists.twi_word.join(' OR ');
+
+				// lists.word = twi_search_word;				
+
+				// alert(twi_search_word);
+
+				// axios.get("./twi.php?q=" + twi_search_word,{'responseType':'json'})
+				// .then(function (response) {
+				// 	console.log(response);				
+				// 	twi_lists.twi_list = response["data"];
+				// 	twi_lists.$forceUpdate();
+				// })
+				// .catch(function (error) {
+				// 	console.log(error);
+				// });
 			})
 			.catch(function (error) {
 				console.log(error);
@@ -106,24 +153,33 @@ if( navigator.geolocation )
 	<div class="row">
 		<div id="list" class="col-lg-4">
 			<div class="card" v-for="list in lists">
-				<img v-if="typeof list.image_url.shop_image1 == 'string' " style="width: 100%; display: block;" v-bind:src="list.image_url.shop_image1" alt="Card image">
-				<img v-else style="height: 200px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22318%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20318%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158bd1d28ef%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158bd1d28ef%22%3E%3Crect%20width%3D%22318%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22129.359375%22%20y%3D%2297.35%22%3EImage%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image">
+				<img v-if="typeof list.image_url.shop_image1 == 'string' " style="height: 150px; margin:10px auto 0; display: block;background: #555;" v-bind:src="list.image_url.shop_image1" alt="Card image">
+				<img v-else style="height: 150px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22318%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20318%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158bd1d28ef%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158bd1d28ef%22%3E%3Crect%20width%3D%22318%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22129.359375%22%20y%3D%2297.35%22%3EImage%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image">
 				
 				<div class="card-block">
-				<h5 class="card-title">{{ list.name }}</h5>
-				</div>
-				<div class="card-block">
-				<p v-if="typeof list.pr.pr_short == 'string' " class="card-text">{{ list.pr.pr_short }}</p>
-				<p v-else></p>
+					<h5 class="card-title">{{ list.name }}</h5>
+					
+					<p v-if="typeof list.pr.pr_short == 'string' " class="card-text">{{ list.pr.pr_short }}</p>
+					<p v-else></p>
 
-				<p class="card-text">{{ list.opentime }}<br>{{ list.address }}<br>{{ list.tel }}</p>	
+					<small class="card-text">{{ list.opentime }}<br>{{ list.address }}<br>{{ list.tel }}</small>	
 				</div>
 				<div class="card-footer text-muted text-xs-center">
 					<a v-bind:href="list.url" target="_blank">ぐるなび店舗ページへ</a>
 				</div>
           </div>
 		</div>
-		<div class="col-lg-6">
+		<div id="twi_lists" class="col-lg-8">
+			<div class="bs-component">
+			<div class="card card-outline-primary text-xs-center" v-for="twi in twi_list">
+            <div class="card-block">
+              	<blockquote class="card-blockquote">
+                <p>{{ twi.text }}</p>
+                <footer>{{ twi.user_name }}</footer>
+              	</blockquote>
+            	</div>
+          	</div>
+			</div>
 		</div>
 	</div>
 </div>
---
+++ twi.php
@@ -0,0 +1,30 @@
+<?php
+ini_set( 'display_errors', 1 );
+ini_set('error_reporting', E_ALL);
+
+require dirname(__FILE__).'./vender/TwistOAuth-master/build/TwistOAuth.phar';
+
+$consumer_key = '6OTBeMZDiN4Sh9HHIv1BVW5cn';
+$consumer_secret = 'wggPRikuqES4odsmRZw78dQCEvVML105lYf6hUo9O7fBgtC0ef';
+$access_token = '936442582132858880-fZ1rE9Lt3s9t2Cb9RwLwZ6yuSxGcG8g';
+$access_token_secret = 'RSoP9lY10mTTSTFaNvnq60aO5YFvFMiixeN6wrRozEIFF';
+
+$connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
+
+
+function h($str){
+    return htmlspecialchars($str, ENT_QUOTES, 'utf-8');
+}
+
+$q = h($_GET['q']);
+// $q = 'サンマルクカフェ 人形町店 OR サンマルクカフェ 小伝馬町駅前店 OR サンドウィッチパーラーまつむら 人形町本店 OR デリフランス 日本橋 OR サンドッグイン 神戸屋 馬喰横山駅店 OR ドイツパンの店 タンネ OR サブウェイ 日本橋兜町店 OR 室町ボンクール本店 OR Signifiant Signifie 日本橋タカシマヤ店 OR サンドイッチハウス メルヘン 髙島屋日本橋店';
+
+// キーワードによるツイート検索
+$tweets_params = ['q' => $q ,'count' => '10'];
+$tweets = $connection->get('search/tweets', $tweets_params)->statuses;
+
+// var_dump($tweets);
+echo json_encode($tweets, true);
+// var_dump($tweets);
+
+// echo "end";
